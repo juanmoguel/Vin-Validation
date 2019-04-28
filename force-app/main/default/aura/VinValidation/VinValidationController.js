3cd8c72fcf6be80da855d814e1ce260078272560
juanmoguel <juanchomoguel@gmail.com>
2019-04-28 17:29:44 -0500

    First approach, The component get the response from the api y you put the vin number and the year

--- force-app/main/default/aura/VinValidation/VinValidationController.js
+++ force-app/main/default/aura/VinValidation/VinValidationController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+    validate: function(component, event, helper) {
+        var vinNumber = component.get("v.vinNumber");
+        var vinYear = component.get("v.year");
+        console.debug(vinNumber + vinYear);
+        helper.getValidation(component, vinNumber, vinYear);
 
     }
 })
--- force-app/main/default/aura/VinValidation/VinValidationHelper.js
+++ force-app/main/default/aura/VinValidation/VinValidationHelper.js
@@ -1,5 +1,18 @@
 ({
-    helperMethod : function() {
+    getValidation: function(component, vinNumber, year) {
+        var theUrl = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/' + vinNumber + '?format=json&modelyear=' + year;
+        var action = component.get("c.getCarInformation");
+        action.setParams({
+            "url": theUrl
+        })
 
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.debug('--------> respuesta : ' + JSON.stringify(response.getReturnValue()));
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
